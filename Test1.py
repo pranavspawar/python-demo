2e50a75774d9a13460c910828e0aa7ce0bdc5289
pranavspawar <104649411+pranavspawar@users.noreply.github.com>
2026-03-29 14:31:52 +0530

    Update Test1.py
    changes in code

--- Test1.py
+++ Test1.py
@@ -1,8 +1,14 @@
-a = 10
-b=5
+a = 50 
+b=90
 if a<b :
   print("b is larger than a")
+  c=a+b
+  print("Addition of two number is a+b" c)
 else :
   print("a is larger than b")
+  c=a-b
+  print("Subtraction of two number is a-b" c)
+
+
 
 
