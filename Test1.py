9c0d09b59969f8acfbc231c68d3e8a1888f9d6f9
pranavspawar <104649411+pranavspawar@users.noreply.github.com>
2026-03-29 14:35:13 +0530

    Update Test1.py
    fix compilation error

--- Test1.py
+++ Test1.py
@@ -3,11 +3,11 @@ b=90
 if a<b :
   print("b is larger than a")
   c=a+b
-  print("Addition of two number is a+b" c)
+  print(c)
 else :
   print("a is larger than b")
   c=a-b
-  print("Subtraction of two number is a-b" c)
+  print(c)
 
 
 
